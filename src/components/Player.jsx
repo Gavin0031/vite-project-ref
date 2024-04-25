1783c9a2dd5878411b2f32c67318b2cae451dbcc
gli32 <gli32@hawk.iit.edu>
2024-04-25 16:47:01 -0400

    Timer

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState, useRef } from "react" ;
+
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [pName, setPName] = useState(null);
+
+  function handleClick() {
+    setPName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {pName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
-    </section>
+    </section> 
   );
 }
